d857b4eca71e565e4ee1013fa37345fd058e3305
jo4086 <ghzktm5892@gmail.com>
2024-11-08 14:22:09 +0900

    ss

--- React_study/myfolder/src/App.js
+++ React_study/myfolder/src/App.js
@@ -1,10 +1,12 @@
 import Chpater4 from './summarize/Chapter4'
-import Chpater6 from './summarize/Chapter6'
+// import Chpater6 from './summarize/Chapter6'
+import ToggleExample from './summarize/Chpater6/ToggleExample'
 
 function App() {
    return (
       // <Chpater4 />
-      <Chpater6 />
+      // <Chpater6 />
+      <ToggleExample />
    )
 }
 
